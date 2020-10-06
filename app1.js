7f8135c7be4a3a569b80db48f662da505b82405a
junya30st <ji06140210@gmail.com>
2020-10-06 21:35:30 +0900

    Update app1.js

--- app1.js
+++ app1.js
@@ -4,50 +4,83 @@
 // 任意の選択肢を表示する
 // ボタンをクリックしたら正誤判定する(if文)
 
-let question = "私は何歳でしょう？"
-let answers = [
-  '28歳',
-  "30歳",
-  "31歳"
+let quiz = [
+  {
+    question: '私は何歳でしょう？',
+    answers: [
+      '29歳', '30歳', '31歳'
+    ],
+    correct: '31歳'
+  },
+  {
+    question: '私の血液型は何でしょう？',
+    answers: [
+      'A', 'B', 'O'
+    ],
+    correct: 'O'
+  },
+  {
+    question: '私の出身はどこでしょう？',
+    answers: [
+      '千葉', '福岡', '東京'
+    ],
+    correct: '福岡'
+  }
 ]
-const correct = "31歳"
-const $button = document.getElementsByTagName('button')
 
+let quizIndex = 0
+// let question = "私は何歳でしょう？"
+// let answers = [
+//   '28歳',
+//   "30歳",
+//   "31歳"
+// ]
+// const correct = "31歳"
+const $button = document.getElementsByTagName('button')
+const buttonLength = $button.length
 // htmlへの代入を全て関数化する
 const setupQuiz = () => {
-  document.getElementById('js-question').textContent = question;
+  document.getElementById('js-question').textContent = quiz[quizIndex].question;
   let buttonIndex = 0
-  let buttonLength = $button.length
   while(buttonIndex < buttonLength){
-    $button[buttonIndex].textContent = answers[buttonIndex];
+    $button[buttonIndex].textContent = quiz[quizIndex].answers[buttonIndex];
     buttonIndex++;
   }
 }
 
 setupQuiz();
 
-const clickHandler = 
-
-$button[0].addEventListener('click', () =>{
-  if (correct === $button[0].textContent){
+const clickHandler = (e) => {
+  if (quiz[quizIndex].correct === e.target.textContent){
     window.alert('正解');
   } else {
     window.alert('不正解');
   }
-})
-$button[1].addEventListener('click', () =>{
-  if (correct === $button[1].textContent){
-    window.alert('正解');
-  } else {
-    window.alert('不正解');
-  }
-})
-$button[2].addEventListener('click', () =>{
-  if (correct === $button[2].textContent){
-    window.alert('正解');
-  } else {
-    window.alert('不正解');
-  }
-})
+};
+
+let handlerIndex = 0;
+while (handlerIndex < buttonLength){
+  $button[handlerIndex].addEventListener('click', (e) =>{
+    clickHandler(e);
+  });
+  handlerIndex++;
+};
+
+// $button[0].addEventListener('click', (e) =>{
+//   clickHandler(e);
+// })
+// $button[1].addEventListener('click', (e) =>{
+//   clickHandler(e);
+// })
+// $button[2].addEventListener('click', (e) =>{
+//   clickHandler(e);
+// })
 
 
+// $button[0].addEventListener('click', (e) =>{
+//   if (quiz[quizIndex].correct === e.target.textContent){
+//     window.alert('正解');
+//   } else {
+//     window.alert('不正解');
+//   }
+// })
